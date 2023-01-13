c0181c9a4d1e96df7be447092111e862f01a698a
dsantosmerino <dsantosmerino@gmail.com>
2023-01-13 10:49:51 +0100

    Bump min Elixir version to 1.10

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpenCagex.Mixfile do
     [
       app: :open_cagex,
       version: "0.1.3",
-      elixir: "~> 1.4",
+      elixir: "~> 1.10",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
